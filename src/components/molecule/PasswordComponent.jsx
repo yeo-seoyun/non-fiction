0371919d224f3b7165283c362378cfff61f2b015
yeoseoyun <yeosummer94@gmail.com>
2024-04-06 17:12:04 +0900

    [Feat] :  비밀번호 입력 조건 적용

--- src/components/molecule/PasswordComponent.jsx
+++ src/components/molecule/PasswordComponent.jsx
@@ -2,18 +2,25 @@ import PropTypes from "prop-types";
 import Input from "../atom/Input";
 import { useAtom } from "jotai";
 import { passwordAtom } from "@/store/stores";
+import { useState } from "react";
 
-// function pwReg(text) {
-//   const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{8,16}$/;
-//   return re.test(String(text).toLowerCase());
-// }
+function pwReg(text) {
+  const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{8,16}$/;
+  return re.test(String(text).toLowerCase());
+}
 
 function PasswordComponent({ id, labelText, placeholder }) {
   const [password, SetPassword] = useAtom(passwordAtom);
-  // const [error, setError] = useState("");
+  const [error, setError] = useState("");
 
   function handleInputChange(e) {
     SetPassword(e.target.value);
+    const { value } = e.target;
+    const isValid = pwReg(value);
+
+    setError(
+      isValid ? "" : "(영문 대소문자/숫자/특수문자 중 2가지 이상 조합, 8~12자)"
+    );
   }
 
   return (
@@ -22,18 +29,21 @@ function PasswordComponent({ id, labelText, placeholder }) {
         id={id}
         type="password"
         labelText={labelText}
-        className="p-2 border border-gray-200"
+        className={`p-2 border ${
+          error ? "border-warning" : "border-gray-200"
+        } focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
         placeholder={placeholder}
         required
         ariaRequired={true}
         value={password}
         onChange={handleInputChange}
       />
-      {id === "password" && (
+      {/* {id === "password" && (
         <p className="text-xs text-gray-100">
           (영문 대소문자/숫자/특수문자 중 2가지 이상 조합, 8~12자)
         </p>
-      )}
+      )} */}
+      {error && <p className="text-xs text-gray-100">{error}</p>}
     </div>
   );
 }
