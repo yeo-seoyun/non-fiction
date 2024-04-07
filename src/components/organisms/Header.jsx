fddf9dc07f591e3476b884666cdbd61692c0006d
yeoseoyun <yeosummer94@gmail.com>
2024-04-07 15:00:31 +0900

    [Feat] :  tablet ver category

--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -3,14 +3,23 @@ import { Link } from "react-router-dom";
 import ProductHeader from "./ProductHeader";
 import { useState } from "react";
 import Cart from "./CartModal";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
   const { isLoggedIn } = useLoginFormStore();
 
   const [isModalOpen, setModalOpen] = useState(false);
-
   const openModal = () => setModalOpen(true);
   const closeModal = () => setModalOpen(false);
+
+  const [isCategoryModalOpen, setCategoryModalOpen] = useState(false);
+  const openCategoryModal = () => setCategoryModalOpen(true);
+  const closeCategoryModal = () => setCategoryModalOpen(false);
+
+  const handleLinkClick = (e) => {
+    closeCategoryModal();
+  };
 
   return (
     <>
@@ -18,8 +27,115 @@ function Header() {
         <header className="w-full px-4 h-[5.625rem] flex items-center justify-between text-xl font-noto-serif font-light">
           <button
             className="lg:hidden bg-bar-icon w-[1.813rem] h-[1.563rem] bg-contain"
-            // onClick={openModal} 카테고리 모달 오픈 로직 구현
+            onClick={openCategoryModal}
           ></button>
+          {isCategoryModalOpen && (
+            <div className="fixed inset-0 z-50 bg-white p-12">
+              <div className="w-full h-full flex flex-col gap-8">
+                <div className="flex justify-end">
+                  <button onClick={closeCategoryModal} className="p-2 text-2xl">
+                    <FontAwesomeIcon icon={faXmark} />
+                  </button>
+                </div>
+                <div className="flex flex-col gap-12">
+                  <ul className="space-y-6 text-3xl">
+                    <li>
+                      <Link
+                        to="/bestseller"
+                        aria-label="베스트 셀러"
+                        className="hover hover:text-gray-100"
+                        onClick={handleLinkClick}
+                      >
+                        Best Seller
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/perfume"
+                        aria-label="향수"
+                        className="hover hover:text-gray-100"
+                        onClick={handleLinkClick}
+                      >
+                        Perfume
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/handandbody"
+                        aria-label="핸드 앤 바디"
+                        className="hover hover:text-gray-100"
+                        onClick={handleLinkClick}
+                      >
+                        Hand & Body
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/comingsoon"
+                        aria-label="홈 프래그런스"
+                        className="hover hover:text-gray-100"
+                        onClick={handleLinkClick}
+                      >
+                        Home Fragrance
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/comingsoon"
+                        aria-label="선물 세트"
+                        className="hover hover:text-gray-100"
+                        onClick={handleLinkClick}
+                      >
+                        Gift Set
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/comingsoon"
+                        aria-label="정보"
+                        className="hover hover:text-gray-100"
+                        onClick={handleLinkClick}
+                      >
+                        About
+                      </Link>
+                    </li>
+                  </ul>
+                  <div className="space-y-2">
+                    <Link
+                      to="/comingsoon"
+                      aria-label="마이 페이지"
+                      onClick={handleLinkClick}
+                    >
+                      My Page
+                    </Link>
+                    <ul className="flex gap-2">
+                      <li>
+                        <Link
+                          to="/login"
+                          aria-label="로그인"
+                          onClick={handleLinkClick}
+                        >
+                          Login
+                        </Link>
+                      </li>
+                      <li>
+                        <span>/</span>
+                      </li>
+                      <li>
+                        <Link
+                          to="/signup"
+                          aria-label="로그인"
+                          onClick={handleLinkClick}
+                        >
+                          Join
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
           <Link
             to="/"
             className="flex items-center lg:bg-nf-logo bg-nfh-logo w-[3.75rem] h-[3.75rem] bg-contain bg-no-repeat bg-center"
