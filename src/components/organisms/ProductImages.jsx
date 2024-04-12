4575ea590720aa0e0470e1d7b294f3ba0a5e43a5
yeoseoyun <yeosummer94@gmail.com>
2024-04-12 15:19:56 +0900

    [Design] :   디테일페이지 tablet ver

--- src/components/organisms/ProductImages.jsx
+++ src/components/organisms/ProductImages.jsx
@@ -4,15 +4,19 @@ import Button from "../atom/Button";
 function ProductImage({ imageUrl, altText, thumbnails }) {
   const [selectedImage, setSelectedImage] = useState(imageUrl);
   return (
-    <div className="flex items-end justify-start gap-4 md:flex-col md:items-start">
+    <div className="flex lg:flex-row lg:items-end justify-start gap-4 md:flex-col md:items-start">
       <div className="w-[80%]">
         <img src={selectedImage} alt={altText} className="w-full" />
       </div>
-      <ul className="flex flex-col justify-between gap-1 md:flex-row">
+      <ul className="flex lg:flex-col justify-between gap-1 md:flex-row">
         {thumbnails?.map((thumb, index) => (
           <li key={index} className="border">
             <Button onClick={() => setSelectedImage(thumb.url)} className="p-1">
-              <img src={thumb.url} alt={thumb.alt} className="w-16 md:w-12" />
+              <img
+                src={thumb.url}
+                alt={thumb.alt}
+                className="lg:w-16 md:w-12"
+              />
             </Button>
           </li>
         ))}
--- src/pages/ProductDetail/ProductDetail.jsx
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -60,55 +60,51 @@ function ProductDetail() {
         <meta charSet="utf-8" />
         <title>NONFICTION | 상세페이지</title>
       </Helmet>
-      <section className="py-[9.375rem] w-full px-28 ">
-        <article className="flex items-start justify-center">
-          <div className="w-[65%] md:w-[55%]">
-            {imageUrl && (
-              <ProductImage
-                imageUrl={imageUrl}
-                altText={productData.title_ko}
-                thumbnails={thumbnails}
-              />
-            )}
-          </div>
-
-          <div className="w-[35%] flex flex-col gap-4 md:w-[45%]">
-            <div>
-              <h1 className="font-noto-serif font-light">
-                {productData.title}
-              </h1>
-              <div className="flex items-center justify-between">
-                <p>{productData.title_ko}</p>
-                <p>{productData.price.toLocaleString()}원</p>
-              </div>
-              <p>{productData.description_ko}</p>
-              <p>{productData.description}</p>
-            </div>
-            <div className="flex flex-col gap-1">
-              <p>Size</p>
-              <p className="w-[6.25rem] border border-black-200 p-2 text-center">
-                100ml
-              </p>
-            </div>
-            <MessageCardSelect
-              selectedOption={selectedOption}
-              handleSelectChange={handleSelectChange}
+      <section className="py-[9.375rem] w-full px-28 flex items-start justify-center">
+        <article className="w-[65%] md:w-[55%]">
+          {imageUrl && (
+            <ProductImage
+              imageUrl={imageUrl}
+              altText={productData.title_ko}
+              thumbnails={thumbnails}
             />
-            <div className="flex gap-2 item-center justify-between">
-              <AddToCartButton
-                product={productData}
-                selectedOption={selectedOption}
-              />
-              <Button
-                className="border border-black-200 p-2 text-center hover:bg-black-200 hover:text-white"
-                type="button"
-                ariaLabel="선물하기"
-              >
-                선물하기
-              </Button>
+          )}
+        </article>
+
+        <article className="w-[35%] flex flex-col gap-4 md:w-[45%]">
+          <div>
+            <h1 className="font-noto-serif font-light">{productData.title}</h1>
+            <div className="flex items-center justify-between">
+              <p>{productData.title_ko}</p>
+              <p>{productData.price.toLocaleString()}원</p>
             </div>
-            <ProductInfo product={productData} />
+            <p>{productData.description_ko}</p>
+            <p>{productData.description}</p>
           </div>
+          <div className="flex flex-col gap-1">
+            <p>Size</p>
+            <p className="w-[6.25rem] border border-black-200 p-2 text-center">
+              100ml
+            </p>
+          </div>
+          <MessageCardSelect
+            selectedOption={selectedOption}
+            handleSelectChange={handleSelectChange}
+          />
+          <div className="flex gap-2 item-center justify-between">
+            <AddToCartButton
+              product={productData}
+              selectedOption={selectedOption}
+            />
+            <Button
+              className="border border-black-200 p-2 text-center hover:bg-black-200 hover:text-white"
+              type="button"
+              ariaLabel="선물하기"
+            >
+              선물하기
+            </Button>
+          </div>
+          <ProductInfo product={productData} />
         </article>
       </section>
     </>
