1401712a43cbf7bae9fb8d9e3d16e2a0714912c9
yeoseoyun <yeosummer94@gmail.com>
2024-05-02 18:54:07 +0900

    [Design] :  스켈레톤 UI 수정

--- src/components/molecule/ProductSkeleton.jsx
+++ src/components/molecule/ProductSkeleton.jsx
@@ -1,8 +1,8 @@
 function ProductSkeleton() {
   return (
     <div className="block w-full cursor-pointer">
-      <li className="w-full flex flex-col justify-between items-center">
-        <div className="w-[325px] h-[325px] bg-gray-300 animate-pulse"></div>
+      <li className="w-full flex flex-col gap-3 justify-between items-center">
+        <div className="w-full h-full p-32 rounded-sm bg-gray-300 animate-pulse"></div>
         <div className="w-full flex flex-col gap-4 p-2">
           <div className="flex justify-between items-center w-full">
             <div className="h-6 bg-gray-200 rounded-md w-[60%] animate-pulse"></div>
